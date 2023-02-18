ea0494fc3d49a13999a721e96eeab982c14c7cf7
Erik Araujo <erik.doctype@gmail.com>
2023-02-18 13:25:26 -0300

    add corrusel and cycle

---
+++ src/components/Carrousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-carousel hide-delimiters cycle height="100vh" show-arrows="hover">
+    <v-carousel-item
+      src="../assets/woman.jpg"
+      cover
+    >
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-h1 font-weight-thin mb-4">
+        WebBoost
+      </h1>
+      <h4 class="subheading">
+        Build your application today!
+      </h4>
+    </div>
+    </v-carousel-item>
+
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+      cover
+    ></v-carousel-item>
+
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      cover
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+<script>
+export default {
+  name: "CarrouselCompornent",
+
+  data() {
+    return {
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card>
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+
+      <v-app-bar color="blue-grey-darken-1" prominent>
+        <v-toolbar-title>My files</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+        class="d-none d-lg-flex d-xl-flex"
+        value="recent"
+        >
+          <v-icon>mdi-home</v-icon>
+
+          Home
+        </v-btn>
+
+        <v-btn
+        class="d-none d-lg-flex d-xl-flex"
+        value="favorites"
+        >
+          <v-icon>mdi-heart</v-icon>
+
+          Serviços
+        </v-btn>
+
+        <v-btn
+        class="d-none d-lg-flex d-xl-flex"
+        value="nearby"
+        >
+          <v-icon>mdi-map-marker</v-icon>
+
+          Localização
+        </v-btn>
+
+        <v-app-bar-nav-icon
+          class="d-md-flex d-lg-none"
+          variant="text"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        class="d-md-flex d-lg-none"
+        v-model="drawer"
+        location="right"
+        temporary
+      >
+        <v-list :items="items"></v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh">
+        <Carrousel />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import Carrousel from "./Carrousel.vue";
+
+export default {
+  name: "NavbarSearch",
+
+  components: {
+    Carrousel,
+  },
+
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      {
+        title: "Home",
+        value: "home",
+      },
+      {
+        title: "Serviços",
+        value: "servicos",
+      },
+      {
+        title: "Localização",
+        value: "localizacao",
+      },
+    ],
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
