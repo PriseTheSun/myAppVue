599aeca78987adf144ad0facdbe56d8f6c80961a
Erik Araujo <erik.doctype@gmail.com>
2023-02-25 23:11:07 -0300

    add descrition on v-dialog

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -7,10 +7,20 @@
             <v-col cols="12" md="6">
               <v-img :src="dialogImage" aspect-ratio="1" cover />
             </v-col>
-            <v-col cols="12" md="6" class="d-flex flex-column justify-start pd-16">
+            <v-col
+              cols="12"
+              md="6"
+              class="d-flex flex-column justify-start pd-16"
+            >
               <v-row class="justify-space-between">
                 <v-card-title>{{ dialogTitle }}</v-card-title>
-                <v-icon class="mr-3 mt-3" icon @click="dialog = false" absolute top right
+                <v-icon
+                  class="mr-3 mt-3"
+                  icon
+                  @click="dialog = false"
+                  absolute
+                  top
+                  right
                   >mdi-close</v-icon
                 >
               </v-row>
@@ -36,17 +46,11 @@
               md="4"
             >
               <v-img
-                :src="image"
+                :src="image.src"
                 :lazy-src="`https://picsum.photos/10/6?image=${index}`"
                 aspect-ratio="1"
                 cover
-                @click="
-                  openDialog(
-                    image,
-                    `Image ${index + 1}`,
-                    `Description for image ${index + 1}`
-                  )
-                "
+                @click="openDialog(image.src, image.title, image.description)"
               />
             </v-col>
           </v-row>
@@ -63,30 +67,106 @@ export default {
     return {
       tab: 1,
       images: [
+        //CATS TAB
         [
-          "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/2173009/pexels-photo-2173009.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 ",
-          "https://images.pexels.com/photos/747795/pexels-photo-747795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/7140797/pexels-photo-7140797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/3859933/pexels-photo-3859933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          {
+            src:"https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato listrado",
+            description: "Este é um gato com lindas listras."
+          },
+          {
+            src:"https://images.pexels.com/photos/2173009/pexels-photo-2173009.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 ",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/747795/pexels-photo-747795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/7140797/pexels-photo-7140797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/3859933/pexels-photo-3859933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },     
         ],
+        //DOGS TAB
         [
-          "https://imgs.search.brave.com/TCenV15AAdyf8ahXSUXxSwfQUKnRWiVX1vwCqEZwfIg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9waXh5/Lm9yZy9zcmMvMC8x/MDQ5LmpwZw",
-          "https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/1739125/pexels-photo-1739125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://imgs.search.brave.com/LjtWbjh8qFx1440U9KAHAR-kloEzT3h6vFgCGNgOrjE/rs:fit:1200:1200:1/g:ce/aHR0cDovL3Bicy50/d2ltZy5jb20vbWVk/aWEvQ2FZb0RBZ1hF/QUl1X0Z5LmpwZzps/YXJnZQ",
-          "https://images.pexels.com/photos/776373/pexels-photo-776373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/5958289/pexels-photo-5958289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          {
+            src:"https://imgs.search.brave.com/TCenV15AAdyf8ahXSUXxSwfQUKnRWiVX1vwCqEZwfIg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9waXh5/Lm9yZy9zcmMvMC8x/MDQ5LmpwZw",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato listrado",
+            description: "Este é um gato com lindas listras."
+          },
+          {
+            src:"https://images.pexels.com/photos/1739125/pexels-photo-1739125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://imgs.search.brave.com/LjtWbjh8qFx1440U9KAHAR-kloEzT3h6vFgCGNgOrjE/rs:fit:1200:1200:1/g:ce/aHR0cDovL3Bicy50/d2ltZy5jb20vbWVk/aWEvQ2FZb0RBZ1hF/QUl1X0Z5LmpwZzps/YXJnZQ",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/776373/pexels-photo-776373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/5958289/pexels-photo-5958289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },     
         ],
-        [
-          "https://images.pexels.com/photos/33152/european-rabbits-bunnies-grass-wildlife.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/10780914/pexels-photo-10780914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/5152010/pexels-photo-5152010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/10780922/pexels-photo-10780922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://www.mulherportuguesa.com/wp-content/uploads/2016/10/coelho-selvagem.jpg",
+         //RABBITS TAB
+         [
+          {
+            src:"https://images.pexels.com/photos/33152/european-rabbits-bunnies-grass-wildlife.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/10780914/pexels-photo-10780914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato listrado",
+            description: "Este é um gato com lindas listras."
+          },
+          {
+            src:"https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/5152010/pexels-photo-5152010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://images.pexels.com/photos/10780922/pexels-photo-10780922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },
+          {
+            src:"https://www.mulherportuguesa.com/wp-content/uploads/2016/10/coelho-selvagem.jpg",
+            title: "Gato fofo",
+            description: "Este é um gato muito fofo."
+          },     
         ],
+
       ],
       dialog: false,
       dialogImage: "",
