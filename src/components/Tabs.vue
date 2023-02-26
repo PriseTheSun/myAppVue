2ad608863939987ab3d5b32e4e1d6fb35e24fd1e
Erik Araujo <erik.doctype@gmail.com>
2023-02-26 00:58:20 -0300

    Add css animations on modal

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -13,9 +13,9 @@
               class="d-flex flex-column justify-start pd-16"
             >
               <v-row class="justify-space-between">
-                <v-card-title>{{ dialogTitle }}</v-card-title>
+                <v-card-title class="fadeInLeft">{{ dialogTitle }}</v-card-title>
                 <v-icon
-                  class="mr-3 mt-3"
+                  class="mr-3 mt-3 rotateIn"
                   icon
                   @click="dialog = false"
                   absolute
@@ -24,7 +24,7 @@
                   >mdi-close</v-icon
                 >
               </v-row>
-              <v-card-text>{{ dialogDescription }}</v-card-text>
+              <v-card-text class="fadeIn">{{ dialogDescription }}</v-card-text>
               <shere-btn/>
 
             </v-col>
@@ -80,32 +80,32 @@ export default {
           {
             src:"https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             title: "Olá sou: Saphira",
-            description: "Uma gata de olhos verdes e pelagem de três cores. Com personalidade carinhosa e brincalhona, ele adora passar horas se divertindo com brinquedos e explorando sua casa. Seu olhar intenso e curioso é capaz de derreter o coração de qualquer um. Não perca a chance de conhecer esse lindo felino!."
+            description: "Saphira é uma gata de olhos verdes e pelagem de três cores. Com personalidade carinhosa e brincalhona, ele adora passar horas se divertindo com brinquedos e explorando sua casa. Seu olhar intenso e curioso é capaz de derreter o coração de qualquer um. Não perca a chance de conhecer esse lindo felino!."
           },
           {
             src:"https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            title: "Gato listrado",
-            description: "Este é um gato com lindas listras."
+            title: "Olá sou: Simba",
+            description: "Simba é um gato rajado, com pelos macios e um olhar penetrante. Ele é muito curioso e adora explorar novos ambientes, mas também é bastante apegado aos seus donos. É brincalhão e adora receber carinho, mas também sabe ser independente. Com seu porte elegante e pelagem marcante, Simba é um gato que chama a atenção por onde passa."
           },
           {
             src:"https://images.pexels.com/photos/2173009/pexels-photo-2173009.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 ",
-            title: "Gato fofo",
-            description: "Este é um gato muito fofo."
+            title: "Olá sou: Max",
+            description: "Max é um gato rajado com olhos verdes penetrantes. Ele é um felino elegante e charmoso, que adora brincar e receber carinho. Seus olhos verdes hipnotizam a todos que o veem e sua pelagem rajada o torna ainda mais bonito e único."
           },
           {
             src:"https://images.pexels.com/photos/747795/pexels-photo-747795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            title: "Gato fofo",
-            description: "Este é um gato muito fofo."
+            title: "Olá sou: Cléo",
+            description: "Cléo é uma gato rajado com olhos comuns. Uma felina curiosa e adora se enfiar em caixas e sacolas. Cléo é muito brincalhona e gosta de receber carinho, principalmente na barriga. Ela é uma companheira fiel e adora dormir ao lado de seus humanos."
           },
           {
             src:"https://images.pexels.com/photos/7140797/pexels-photo-7140797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            title: "Gato fofo",
-            description: "Este é um gato muito fofo."
+            title: "Olá sou: Neve",
+            description: "Neve é um gato branco com manchas rajadas em sua pelagem sedosa. Seus olhos comuns combinam perfeitamente com sua personalidade tranquila e afetuosa. Com uma aparência delicada e elegante, Neve conquista o coração de todos que o conhecem."
           },
           {
             src:"https://images.pexels.com/photos/3859933/pexels-photo-3859933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            title: "Gato fofo",
-            description: "Este é um gato muito fofo."
+            title: "Olá sou: Rajah",
+            description: "Rajah é um gato rajado que lembra um pequeno tigre, com pelagem listrada em tons de marrom e laranja. Seus olhos são comuns, mas parecem penetrar na alma de quem o olha. Rajah é um gato arisco, que prefere ficar na dele e observar o ambiente ao seu redor antes de se aproximar das pessoas."
           },     
         ],
         //DOGS TAB
